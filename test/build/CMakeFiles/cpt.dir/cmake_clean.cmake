ab8f2a21b6006cbc2c1c284befbabe92702f4c30
The-Lyc <liuyichen626@gmail.com>
2023-12-20 11:56:21 +0800

    完成了基本的备份功能

---
+++ test/build/CMakeFiles/cpt.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpt.dir/copy_test.cpp.o"
+  "CMakeFiles/cpt.dir/copy_test.cpp.o.d"
+  "CMakeFiles/cpt.dir/home/lyc/cpp_ws/file_backup/src/copy.cpp.o"
+  "CMakeFiles/cpt.dir/home/lyc/cpp_ws/file_backup/src/copy.cpp.o.d"
+  "cpt"
+  "cpt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
